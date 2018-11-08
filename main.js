1372642deac10bf18b3f330ffc879eea42eb4328
Tucker <trblackw@ncsu.edu>
2018-11-08 09:50:33 -0500

    cleaned up code; added timeFormatter function for handling timestamps

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
 const API_KEY = "dc9c662a1bda3caf8b7c91b83968d8db";
-
 const searchBtn = document.querySelector("button#search");
 const citySearchInput = document.querySelector("input[aria-label=City");
 
-// const generateHTML = data => {
-//   const rawTemplate = document.querySelector("#weather-template").innerHTML;
-//   const compiledTemplate = Handlebars.compile(rawTemplate);
-//   console.log(compiledTemplate(data));
-//   //   const generatedHTML = compiledTemplate(data);
-//   //   const container = document.querySelector("#weather-data");
-//   //   return (container.innerHTML = generatedHTML);
-// };
-
 const searchByCity = async city => {
   const cityWeatherRes = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city.toLowerCase()}&APPID=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city.toLowerCase()}&units=imperial&APPID=${API_KEY}`
   );
   const cityData = await cityWeatherRes.json();
   const { weather, coord, main, wind, sys: time } = cityData;
-  //   const weatherObj = {
-  //     data: [{ city }, ...weather, coord, main, wind, time]
-  //   };
+
   const weatherObj = {
     city,
     weather,
@@ -29,16 +17,32 @@ const searchByCity = async city => {
     wind,
     time
   };
-  console.log(weatherObj.main);
+  console.log(weatherObj);
   const source = document.querySelector("#weather-template").innerHTML;
 
   const template = Handlebars.compile(source);
   const HTML = template(weatherObj);
-  //   console.log(HTML);
   const weatherUI = document.querySelector("#weather-data");
+  weatherUI.style.display = "inherit";
   weatherUI.innerHTML = HTML;
 };
 
 searchBtn.addEventListener("click", () => {
   searchByCity(citySearchInput.value);
+});
+
+//timeformatter snagged (and modified) from Stack Overflow
+//https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript/847196#847196
+
+Handlebars.registerHelper("formatTime", timeStamp => {
+  const date = new Date(timeStamp * 1000);
+  // Hours part from the timestamp
+  const hours = date.getHours();
+  // Minutes part from the timestamp
+  const minutes = `0${date.getMinutes()}`;
+  // Seconds part from the timestamp
+  const seconds = `0${date.getSeconds()}`;
+  // Will display time in 10:30:23 format
+  const formattedTime = `${hours}:${minutes.substr(-1)}:${seconds.substr(-2)}`;
+  return formattedTime;
 });
