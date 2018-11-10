1929c0f32296dee930723c72f34850c5a7da11e1
Tucker <trblackw@ncsu.edu>
2018-11-10 16:06:08 -0500

    built an express server and endpoint to send country json to client

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+let countries = [];
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get("/codes", (req, res) => {
+  res.send(countries);
+});
+
+const port = process.env.PORT || 5000;
+
+fs.readFile("../countries.json", "utf8", (err, data) => {
+  if (err) throw err;
+  countries = JSON.parse(data);
+  console.log(
+    `Worked! countries array now includes ${countries.length} countries`
+  );
+});
+
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
